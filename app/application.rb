c3a4e3b33f8e4af7649cf9f5524f0f7be890db3c
Conor Hamilton <conhamil19@gmail.com>
2018-07-10 17:47:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
           resp.write "Your cart is empty"
         end
     elsif req.path.match(/add/)
-      req
+      add_item =
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
