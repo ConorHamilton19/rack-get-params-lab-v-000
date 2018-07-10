b5bd48365c6393b88e8d44e1f207de336a9414d4
Conor Hamilton <conhamil19@gmail.com>
2018-07-10 17:49:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,11 @@ class Application
         end
     elsif req.path.match(/add/)
       add_item = require.params["item"]
-      
+        if @@items.include? add_item
+         @@cart << add_item
+         resp.write "added #{add_item}"
+        else
+         resp.write "We don't have that item!"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
